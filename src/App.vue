d5c064f6cbaf6ca16e21b9956918ce6dc1ea6f5b
aprillebalsom <aprillebalsom@gmail.com>
2021-05-14 23:06:20 -0400

    added + styled header

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav" class="wrapper">
-      <router-link to="/shop">shop</router-link> 
-      <router-link to="/info">why sustainability?</router-link> 
-      <!-- TODO update logo so colors are in proper order -->
-      <router-link to="/"><img class="logo" src="./assets/logo.png" alt="thank u, mama earth logo"></router-link>  
-      <router-link to="/fun">fun stuff</router-link>
-      <router-link to="/about">about</router-link>
+    <div id="app">
+
+        <div id="nav" class="wrapper">
+        <router-link to="/shop">shop</router-link> 
+        <router-link to="/info">why sustainable?</router-link> 
+        <!-- TODO update logo so colors are in proper order -->
+        <router-link to="/"><img class="logo" src="./assets/logo.png" alt="thank u, mama earth logo"></router-link>  
+        <router-link to="/fun">fun stuff</router-link>
+        <router-link to="/about">about</router-link>
+        </div>
+
+        <router-view class="wrapper" />
+
+        <footer>
+            <section class="main-footer wrapper">
+                <div class="footer-nav">
+                    <ul> 
+                        <li>
+                            <router-link to="/">home</router-link>  
+                        </li>
+                        <li>
+                            <router-link to="/shop">shop</router-link> 
+                        </li>
+                        <li>
+                            <router-link to="/info">why sustainability?</router-link> 
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li>
+                            <router-link to="/fun">fun stuff</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/about">about</router-link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="footer-socials">
+                    <h4>Stay In Touch</h4>
+                    <ul>
+                        <li><a target="_blank" rel="noopener" href="https://www.instagram.com/thankumamaearth/">
+                            <font-awesome-icon :icon="['fab', 'instagram']" />
+                        </a></li>
+
+                        <li><a target="_blank" rel="noopener" href="https://www.facebook.com/thankumamaearth">
+                            <font-awesome-icon :icon="['fab', 'facebook-f']" />
+                        </a></li>
+                        <li><a target="_blank" rel="noopener" href="https://www.pinterest.ca/thankumamaearth/_created/">
+                            <font-awesome-icon :icon="['fab', 'pinterest']" />
+                        </a></li>
+                        <li><a target="_blank" rel="noopener" href="https://www.etsy.com/ca/shop/sundayhomework">
+                            <font-awesome-icon :icon="['fab', 'etsy']" />
+                        </a></li>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="sub-footer">
+                <div class="wrapper">
+                    <p>Designed + developed by <a href="https://www.etsy.com/ca/shop/sundayhomework" target="_blank" rel="noopener">sunday homework</a> | &copy; 2021 <em>thank u,</em> mama earth</p>
+                </div>
+            </section>
+
+        </footer>
+
     </div>
-
-    <router-view class="wrapper" />
-
-    <footer>
-      <section class="main-footer wrapper">
-        <div class="footer-nav">
-
-        </div>
-        <div class="footer-socials">
-          <h4>Stay In Touch</h4>
-          <ul>
-            
-            <li><a target="_blank" rel="noopener" href="https://www.instagram.com/thankumamaearth/">
-              <font-awesome-icon :icon="['fab', 'instagram']" />
-            </a></li>
-
-            <li><a target="_blank" rel="noopener" href="https://www.facebook.com/thankumamaearth">
-              <font-awesome-icon :icon="['fab', 'facebook-f']" />
-            </a></li>
-            <li><a target="_blank" rel="noopener" href="https://www.pinterest.ca/thankumamaearth/_created/">
-              <font-awesome-icon :icon="['fab', 'pinterest']" />
-            </a></li>
-            <li><a target="_blank" rel="noopener" href="https://www.etsy.com/ca/shop/sundayhomework">
-              <font-awesome-icon :icon="['fab', 'etsy']" />
-            </a></li>
-          </ul>
-
-        </div>
-        
-      </section>
-
-      <section class="sub-footer wrapper">
-        <p>Designed + developed by <a href="https://www.etsy.com/ca/shop/sundayhomework" target="_blank" rel="noopener">sunday homework</a> | &copy; 2021 <em>thank u,</em> mama earth</p>
-      </section>
-
-    </footer>
-  </div>
 </template>
 
 
@@ -54,23 +75,57 @@
 
 
 #nav {
-  text-align: center;
+    text-align: center;
 
-  padding: 30px;
+    padding: 30px;
   
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 
-  a {
-    margin: 0 15px;
+    a {
+        margin: 0 15px;
     
-    &.router-link-exact-active {
-      color: $green;
+        &.router-link-exact-active {
+            color: $green;
+        }
     }
-  }
 }
 
-footer {
-  background-color: $green;
+.main-footer {
+    font-size: 1.4rem;
+    line-height: 2.2rem;
+
+    padding: 20px 0;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.footer-nav,
+.footer-socials ul {
+    display: flex;
+}
+
+.footer-nav ul{
+    margin-right: 25px;
+}
+
+.footer-socials li{
+    margin: 10px 15px 10px 0;
+}
+
+
+.sub-footer {
+    background-color: $green;
+
+    text-align: center;
+
+    padding: 10px 0;
+
+    p,
+    a {
+        font-size: 1.4rem;
+    }
 }
 </style>
